4b9daa2e0b2e7d1ca01de792b10a69de481eb18a
Sohardh <31403178+Sohardh@users.noreply.github.com>
2018-06-18 12:27:06 +0530

    Initial Commit

---
+++ Average Calculator for iOS 11/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Average Calculator for iOS 11
+//
+//  Created by Sohardh Chobera on 18/06/18.
+//  Copyright © 2018 Sohardh Chobera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
